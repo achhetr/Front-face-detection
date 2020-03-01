ef1e1811208e593348f558142b2ecd808ea21109
Akash Chhetri <akash.chhetri1@gmail.com>
2020-03-01 22:22:46 +1100

    04 - Add Motion Detection

---
+++ motion-detection.py
@@ -0,0 +1,57 @@
+import cv2
+import time
+
+# web cam
+video = cv2.VideoCapture(0)
+
+# first frame
+first_frame = None
+
+
+while True:
+    video = cv2.VideoCapture(0)
+    check, frame = video.read()
+
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame,(21,21),0)
+
+    ###
+    ########## detect motion
+    ###
+
+    # get first frame
+    if first_frame is None:
+        first_frame = gray_frame
+        continue
+    
+    diff_frame = cv2.absdiff(first_frame,gray_frame)
+    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    # find contours
+    (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000 :
+            continue
+        
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),6)
+
+    # add rectangle in the matching area
+    #    for x,y,w,h in profile_faces:
+    #    frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),6)
+
+    
+    cv2.imshow("Capturing",gray_frame)
+    cv2.imshow("Delta",diff_frame)
+    cv2.imshow("THRES",thresh_frame)
+    cv2.imshow("Contor",frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
